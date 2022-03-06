963286690b0ebab727eace8cfb88d3935a7857a1
Romain Delaunay <delaunay.romain96@gmail.com>
2022-03-06 15:31:07 -0300

    return fromn crash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  before_action :require_user, except: [:show, :index]
-  before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
   def show
   end
@@ -19,7 +18,6 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -49,14 +47,8 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description, category_ids: [])
+    params.require(:article).permit(:title, :description)
   end
 
-  def require_same_user
-    if current_user != @article.user && !current_user.admin?
-      flash[:alert] = "You can only edit or delete your own article"
-      redirect_to @article
-    end
-  end
 
 end
